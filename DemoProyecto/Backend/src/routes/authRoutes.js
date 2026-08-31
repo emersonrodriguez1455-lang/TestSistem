b60c9ac6e710f10f39a0102be9e935a29705cd45
emersonrodriguez1455-lang <emersonrodriguez1455@gmail.com>
2026-08-30 21:54:24 -0600

    Update authRoutes.js
    cambios en rutas

--- DemoProyecto/Backend/src/routes/authRoutes.js
+++ DemoProyecto/Backend/src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const authController = require('../controllers/authController')
 const { requireAuth } = require('../middlewares/auth')
+const validate = require('../middlewares/validate')
+const { loginSchema } = require('../validators/schemas')
 
 const router = Router()
 
-router.post('/login', authController.login)
+router.post('/login', validate(loginSchema), authController.login)
 router.post('/logout', requireAuth, authController.logout)
 router.get('/me', requireAuth, authController.me)
 
