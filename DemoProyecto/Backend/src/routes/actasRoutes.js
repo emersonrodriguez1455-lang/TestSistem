0d15bed4de83cc9773b2a4dc6dedd44847861e8e
emersonrodriguez1455-lang <emersonrodriguez1455@gmail.com>
2026-08-30 21:54:55 -0600

    Update actasRoutes.js

--- DemoProyecto/Backend/src/routes/actasRoutes.js
+++ DemoProyecto/Backend/src/routes/actasRoutes.js
@@ -1,18 +1,20 @@
 const { Router } = require('express')
 const actasController = require('../controllers/actasController')
 const { requireAuth } = require('../middlewares/auth')
+const validate = require('../middlewares/validate')
+const { crearActaSchema, editarActaSchema } = require('../validators/schemas')
 
 const router = Router()
 
 // Todas las rutas de actas requieren autenticación
 router.use(requireAuth)
 
-router.post('/', actasController.crearActa)
+router.post('/', validate(crearActaSchema), actasController.crearActa)
 router.get('/', actasController.listarActas)
 router.post('/exportar/excel', actasController.exportarExcel)
 router.get('/:id', actasController.obtenerActa)
 router.get('/:id/pdf', actasController.generarPdf)
-router.put('/:id', actasController.editarActa)
+router.put('/:id', validate(editarActaSchema), actasController.editarActa)
 router.delete('/:id', actasController.eliminarActa)
 router.post('/:id/firma', actasController.guardarFirma)
 router.delete('/:id/firma', actasController.reiniciarFirma)
